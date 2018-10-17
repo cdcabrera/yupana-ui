ff1ea5d91576f860289233f67cbaed34cefdf2b7
Chris Hambridge <chambrid@redhat.com>
2018-10-17 13:46:11 -0400

    Setup travis deployments.

--- .travis/after_success.sh
+++ .travis/after_success.sh
@@ -6,10 +6,10 @@ if [ "${TRAVIS_PULL_REQUEST}" != "false" ]; then
     echo -e "Pull Request, not pushing a build"
     exit 0;
 else
-    openssl aes-256-cbc -K $encrypted_ca9080c40340_key -iv $encrypted_ca9080c40340_iv -in starter.enc -out starter -d
-    chmod 600 starter
+    openssl aes-256-cbc -K $encrypted_6057e226967d_key -iv $encrypted_6057e226967d_iv -in yupana_ui_rsa.enc -out yupana_ui_rsa -d
+    chmod 600 yupana_ui_rsa
     eval `ssh-agent -s`
-    ssh-add starter
+    ssh-add yupana_ui_rsa
 fi
 
 # If current dev branch is master, push to build repo ci-beta
